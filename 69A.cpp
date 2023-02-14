7a22feca5eb2291c3d87d8ce7ad4950bdfd06fba
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2023-02-15 01:04:44 +0600

    Add files via upload

---
+++ 69A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   int n;
+cin>>n;
+    int x,y,z;
+    int sum1=0,sum2=0,sum3=0;
+    while(n--){
+        cin>>x>>y>>z;
+        sum1+=x;
+        sum2+=y;
+        sum3+=z;
+    }
+    if(sum1==0&&sum2==0&&sum3==0){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    
+     return 0;
+}
